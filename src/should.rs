49e54c522ed330876b09e913d7d0a8d5c684c1c7
la10736 <michele.damico@gmail.com>
2024-03-21 16:30:28 +0100

    Tests against some vk

--- src/should.rs
+++ src/should.rs
@@ -21,23 +21,9 @@ use crate::macros::u256s;
 
 use super::*;
 
-trait TestCase {
-    const PROOF: ProofData;
-    const PUBS: U256;
-
-    fn valid_proof() -> Proof {
-        Self::PROOF.try_into().unwrap()
-    }
-
-    fn valid_pubs() -> Public {
-        Self::PUBS.into()
-    }
-}
-
-struct ValidTestCase {}
-
-impl TestCase for ValidTestCase {
-    const PROOF: ProofData = u256s![
+#[fixture]
+fn valid_proof() -> Proof {
+    ProofData::try_from(u256s![
         "283e3f25323d02dabdb94a897dc2697a3b930d8781381ec574af89a201a91d5a",
         "2c2808c59f5c736ff728eedfea58effc2443722e78b2eb4e6759a278e9246d60",
         "0f9c56dc88e043ce0b90c402e96b1f4b1a246f4d0d69a4c340bc910e1f2fd805",
@@ -62,62 +48,24 @@ impl TestCase for ValidTestCase {
         "20e4c02f5a71082a8bcf5be0b5750a244bd040a776ec541dfc2c8ae73180e924",
         "0ada5414d66387211eec80d7d9d48498efa1e646d64bb1bf8775b3796a9fd0bf",
         "0fdf8244018ce57b018c093e2f75ed77d8dbdb1a7b60a2da671de2efe5f6b9d7",
-    ];
-
-    const PUBS: U256 = u256!("0d69b94acdfaca5bacc248a60b35b925a2374644ce0c1205db68228c8921d9d9");
-}
-
-struct AdditionalValidTestCase {}
-
-impl TestCase for AdditionalValidTestCase {
-    const PROOF: ProofData = u256s![
-        "2ecc31435ec6d6963d463c38ea5662d9c94a67e441e7bc611598ebcc59f57188",
-        "0768291fd5d95fcf02bce7e4fde1f048b843bbffab1f242904e82d443a4ebb61",
-        "150c3a4afdbb62d034320da390e3585a30ba13f4df73798b78e5a75655d3350d",
-        "19fca02cc5838405f9ae4177ac7117971af2cb5006d7a46436f644410d6e7c52",
-        "099f803c0f18d4b44fe22f3100d1fc80ccb7309fa7168f51bc64f3fc0f1dbd24",
-        "0b0573f3593238e56b23e75246d9d0f6f6a5cf824700667e3482ca9fecf74cdc",
-        "0b308e6a8f69dccb9ca00d540543441f7030928da766406a152427bdf31b4405",
-        "1b6b5198b34006f9ac34c6c857e450cc11f5c6b6d21119fe283738581c0ad8bd",
-        "0f8cbdbc574f64da884f6a02e00669f3eef10138266f3d7fa278aef1b1c60171",
-        "005c0c2b8b2429c5003c5ab24af44cb1ab81cdc96dcaf6004a0f74406bb10f45",
-        "233b13015cef8c40c491a7770efd0a8d8a64186d4f3827e74972bfc25b11f1f0",
-        "02550a5e253c923c5783026c7439601595477f1a212de449c64a8ae5e2fc0313",
-        "127bf9cd5146217e531196ce65ccef3249375450d6932151f923c39e6a735882",
-        "23d90f5bf230eee5a6cb6463f161602cc37fe538e2954ebef695b926b76e3fae",
-        "299c60c1952aa4b1f246204ac7c22c0156ede30aeb73444ee40d69c0f131fa47",
-        "1fdca090abfd38541c88ee73624657a695155748643f7834b80d1c0481079e67",
-        "0033817252b24575a4e6007f08f37c34462d5e9fd50b1e83ec8cfc86149400d5",
-        "19e224ee11831ac393e3a09730be6f385ae5c9e14446fde5069fea751fb6b482",
-        "11c85268b8017de7981eb1bd78526bc20d5f863ad3abe249728ca7b75b2146c1",
-        "254465d6100a911213d95f800779e74f6701b1dfa0b6660642108fd2c7cd2f13",
-        "1d9163eeebe9d8aabdf8d37fde4451f762be478d117688e0a6ed2648dbe025e8",
-        "2a4b13ee629a73d1efa6f269747506058746aa589bb961c1385bb2b30e0086f0",
-        "10ef87535f2137a04f19fe5aa7c4f348c32ce6f5b0b45bb503895673a8a51d7f",
-        "1b0228693fbfb38be718b04c9fdf116a97d7f30e670db84d21bb0d12fc576454",
-    ];
-
-    const PUBS: U256 = u256!("15950a3fab52ee1557ac7b895deeca2eb27bacfc3b9e26a39b1875149680611d");
-}
-
-#[fixture]
-fn valid_proof() -> Proof {
-    ValidTestCase::valid_proof()
+    ])
+    .unwrap()
+    .try_into()
+    .unwrap()
 }
 
 #[fixture]
 fn valid_pubs() -> Public {
-    ValidTestCase::valid_pubs()
+    u256!("0d69b94acdfaca5bacc248a60b35b925a2374644ce0c1205db68228c8921d9d9").into()
 }
 
 #[fixture]
-fn valid_vk() -> VerificationKey {
+fn vk() -> VerificationKey {
     VerificationKey::default()
 }
 
 #[rstest]
-fn compute_valid_check_paring(valid_proof: Proof, valid_pubs: Public) {
-    let vk = VerificationKey::default();
+fn compute_valid_check_paring(vk: VerificationKey, valid_proof: Proof, valid_pubs: Public) {
     let vk_data = (&vk).into();
 
     let challenges = Challenges::build(&vk_data, &valid_proof, &valid_pubs);
@@ -144,13 +92,38 @@ fn compute_valid_check_paring(valid_proof: Proof, valid_pubs: Public) {
 }
 
 #[rstest]
-#[case(ValidTestCase {})]
-#[case(AdditionalValidTestCase {})]
-fn verify_valid_proof<TC: TestCase>(#[case] _a: TC) {
-    let key = VerificationKey::default();
-    assert!(verify(&key, &TC::valid_proof(), &TC::valid_pubs()).is_ok())
+fn verify_valid_proof(vk: VerificationKey, valid_proof: Proof, valid_pubs: Public) {
+    assert!(verify(&vk, &valid_proof, &valid_pubs).is_ok())
 }
 
+#[cfg(feature = "std")]
+#[cfg(feature = "serde")]
+mod verify_valid_deserialized_proof {
+    use serde::Deserialize;
+
+    use super::*;
+    use std::path::PathBuf;
+
+    #[rstest]
+    fn from_given_files(#[files("resources/proves/*.json")] path: PathBuf) {
+        #[derive(Deserialize)]
+        struct Data {
+            proof: String,
+            pubs: String,
+            vk: VerificationKey,
+        }
+
+        let Data { proof, pubs, vk } =
+            serde_json::from_reader(std::fs::File::open(path).unwrap()).unwrap();
+        let proof: Proof = (&<ProofRawData>::try_from(hex::decode(proof).unwrap()).unwrap())
+            .try_into()
+            .unwrap();
+        let pubs: Public = <[u8; 32]>::try_from(hex::decode(pubs).unwrap())
+            .unwrap()
+            .into();
+        verify(&vk, &proof, &pubs).unwrap()
+    }
+}
 mod reject {
     use self::proof_input::ProofFields;
 
@@ -234,6 +207,7 @@ mod reject {
     #[rstest]
     fn an_invalid_proof(
         mut rng: impl Rng,
+        vk: VerificationKey,
         valid_proof: Proof,
         valid_pubs: Public,
         #[values(
@@ -241,7 +215,6 @@ mod reject {
         )]
         change: ProofFields,
     ) {
-        let vk = VerificationKey::default();
         let perturbed_proof = change.perturbed(valid_proof, &mut rng);
 
         assert!(verify(&vk, &perturbed_proof, &valid_pubs).is_err())
@@ -310,10 +283,10 @@ mod reject {
         mut rng: impl Rng,
         valid_proof: Proof,
         valid_pubs: Public,
-        valid_vk: VerificationKey,
+        vk: VerificationKey,
         #[values(Power, K1, K2, W, W3, W4, W8, Wr, X2, C0)] change: VkFields,
     ) {
-        let perturbed_vk = change.perturbed(valid_vk, &mut rng);
+        let perturbed_vk = change.perturbed(vk, &mut rng);
 
         assert!(verify(&perturbed_vk, &valid_proof, &valid_pubs).is_err())
     }
