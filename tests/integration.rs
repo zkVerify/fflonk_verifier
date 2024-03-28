ed74fb5eccb2af07cb178823b04e71af3a129a7a
Michele d'Amico <michele.damico@gmail.com>
2024-03-28 12:08:58 +0100

    License and copyright (#7)

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,3 +1,19 @@
+// Copyright 2024, The Horizen Foundation
+// Copyright 2021 0KIMS association.
+//
+// fflonk_verifier is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// fflonk_verifier is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with fflonk_verifier.  If not, see <http://www.gnu.org/licenses/>.
+
 use fflonk_verifier::Proof;
 use hex_literal::hex;
 
